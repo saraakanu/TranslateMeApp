f4f409ec1fe6a0c148515084a31f656e223135cc
saraakanu <sara.kanu@bison.howard.edu>
2026-04-16 18:48:44 -0400

    Initial Commit

---
+++ TranslateMeApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TranslateMeApp
+//
+//  Created by Sara Kanu on 4/16/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TranslateMeApp/TranslateMeAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TranslateMeAppApp.swift
+//  TranslateMeApp
+//
+//  Created by Sara Kanu on 4/16/26.
+//
+
+import SwiftUI
+
+@main
+struct TranslateMeAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
